152ebc08b4a7b4cff2565e814ae0f8a5f5e5189e
Arjun sharmaa I <rjnsharma344@gmail.com>
2017-02-14 12:12:52 +0000

    Delete upload.js

--- server/l-o/down/upload.js
+++
@@ -1,6 +0,0 @@
-/*
- *LOGIN ONCE https://github.com/rjnsharma344/login-once
- *@desc to add file uploading capacity
-        drive file browser::http://bit.ly/2jVE23L
- *@lastedit MON 16 JAN 2017
-*/
