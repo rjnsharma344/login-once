734efcf0fde62d28ff9b45d03b95cc585c3cae83
Arjun sharmaa I <rjnsharma344@gmail.com>
2017-02-14 13:05:13 +0000

    Removed unwanted chrome code

--- Firefox/background.js
+++ Firefox/background.js
@@ -13,27 +13,6 @@
 chrome.cookies.onChanged.addListener(function(info) {
   console.log("onChanged" + JSON.stringify(info));
 });
-// Tab changer BUILT IN
-function focusOrCreateTab(url) {
-  chrome.windows.getAll({"populate":true}, function(windows) {
-    var existing_tab = null;
-    for (var i in windows) {
-      var tabs = windows[i].tabs;
-      for (var j in tabs) {
-        var tab = tabs[j];
-        if (tab.url == url) {
-          existing_tab = tab;
-          break;
-        }
-      }
-    }
-    if (existing_tab) {
-      chrome.tabs.update(existing_tab.id, {"selected":true});
-    } else {
-      chrome.tabs.create({"url":url, "selected":true});
-    }
-  });
-}
 // Extension click listner BUIlT IN
 function openPage() {
   browser.tabs.create({
